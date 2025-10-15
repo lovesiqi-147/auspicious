34bece8809091a9ccbb3c6a76cca877129facb91
lovesiqi-147 <2128526951@qq.com>
2025-10-15 23:47:37 +0800

    Add files via upload

---
+++ GPIO.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+typedef enum{
+	GPIO_Speed_2MHz,
+	GPIO_Speed_10MHz,
+	GPIO_Speed_50MHz
+} GPIO_Speed_Type;
+
+typedef struct{
+	GPIO_Speed_Type GPIO_Speed;
+} GPIO_InitTypeDef;
+
+void GPIO_InitStructureInit(GPIO_InitTypeDef*GPIO_InitStruct){
+	if (GPIO_InitStruct != NULL){
+	    GPIO_InitStruct->GPIO_Speed = GPIO_Speed_2MHz;
+	}
+
+}
+
+int main() {
+	GPIO_InitTypeDef GPIO_InitStructure;
+	GPIO_InitStructureInit(&GPIO_InitStructure);
+	printf("GPIO Speed: 2 MHz\n");
+	
+	return 0;	
+}
